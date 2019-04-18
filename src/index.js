bbd41e52754bceda4b63468f686fbe35d1242451
jisung594 <jisung594@gmail.com>
2019-04-18 15:20:44 -0400

    log

--- src/index.js
+++ src/index.js
@@ -96,6 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return 1
           }
         })
+        console.log(sortedBeers)
 
         let styleHeader = document.createElement('h2')
         styleHeader.setAttribute('class', 'index-header')
